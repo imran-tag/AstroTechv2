9f458f55b166a6a74b398f5ece257caff5218b80
imran-tag <immranounamir@gmail.com>
2026-04-26 12:54:35 +0200

    set production API URLs to VPS

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
-  apiURL: "http://213.32.60.67:4000/api/v1"
-  //apiURL: "http://213.32.60.67:7777/api/v1"
+  apiURL: "http://178.104.195.152:3005/api/v1",
+
+  url_client: "http://178.104.195.152:3001/api/v1",
+  url_fichier: "http://178.104.195.152:3002/api/v1",
+  url_referent: "http://178.104.195.152:3003/api/v1",
+  url_technicien: "http://178.104.195.152:3004/api/v1",
+  url_affaire: "http://178.104.195.152:3005/api/v1",
+  url_affaire_uploads: "http://178.104.195.152:3005/uploads",
+  url_upload: "http://178.104.195.152:3002/uploads"
 };
